6726773523cd96ef333c3f5dd2337760acc5e7bd
Josh Mervine <jmervine@yp.com>
2013-03-05 14:38:53 -0800

    fixing typo

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      "Installs/Configures httperf"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 depends          "build_essential"
-dpeneds          "git"
+depends          "git"
