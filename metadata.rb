3d9373fdfe9b18f974e0c9186dce6a4e2d5bd363
Josh Mervine <jmervine@yp.com>
2013-03-05 14:37:38 -0800

    adding dependancies

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          "All rights reserved"
 description      "Installs/Configures httperf"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+depends          "build_essential"
+dpeneds          "git"
