a7b9b22b9c61b77bc7abd650f9210fdd62004f99
Josh Mervine <jmervine@yp.com>
2013-03-09 14:31:55 -0800

    updating version for proper defaults

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license          "MIT"
 description      "Installs/Configures httperf"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 recipe           "httperf::default", "Builds httperf from github.com/rubyops/httperf source."
-version          "0.1.0"
+version          "0.1.1"
 depends          "build-essential"
 depends          "git"
